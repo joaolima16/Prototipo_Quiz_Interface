c4894f62788cb7e8410e01523a78b45bdb9513ad
joao vitor de lima gonçalves <88281625+joaolima16@users.noreply.github.com>
2023-04-24 21:01:42 -0300

    Add files via upload

---
+++ src/main/java/Questions/Questions.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Questions;
+
+/**
+ *
+ * @author João Vitor
+ */
+
+
+public final class Questions  {
+	private String correctResponse;
+	private String response;
+	 
+	 public Questions(String correctResponse, String response) {
+	    setCorrectResponse(correctResponse);
+	    setOtherResponses(response);
+	  }
+	  public void setCorrectResponse(String response){
+	     this.correctResponse = response;
+	  }
+	  
+	  public String getCorrectResponse() {
+	    return correctResponse;
+	  }
+	  
+	  public void setOtherResponses(String response){
+		  this.response = response;
+		
+	  }
+	  
+	  public String getResponse() {
+		 return response; 
+	  }
+	  public String verifyResponse() {
+			  if(correctResponse.equalsIgnoreCase(getResponse())) {
+				 return "correta";
+			  }
+			  else {
+				return "incorreta";
+			  }	  
+		
+	  }
+	  
+	  
+}
+
